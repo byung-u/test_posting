4bede7091337f1b6979f95a84fa085e2235aa9be
byung-u <iam.byungwoo@gmail.com>
2017-12-19 16:15:44 +0900

    Add finlife

--- blog_post/dexa.py
+++ blog_post/dexa.py
@@ -165,18 +165,37 @@ class DailyLifeAndPost:
             ret = m.endswith('0000000')
             if ret is True:
                 return m[0:-7] + '천만원'
+
+            ret = m.endswith('000000')
+            if ret is True:
+                return m[0:-6] + '백만원'
+
+            ret = m.endswith('00000')
+            if ret is True:
+                return m[0:-5] + '십만원'
             else:
                 return m + '원'
 
-    def fixed_deposit(self, bp):
+    def get_fixed_deposit(self, bp, gcode, group):
+        if group == '은행':
+            code_info = BANK_CODE
+        elif group == '저축은행':
+            code_info = SAVINGS_BANK_CODE
+        else:
+            bp.logger.error('Invalid group %s', group)
+            return None
+
         result = '금융감독원의 금융상품통합 비교공시 정보를 바탕으로 작성된 글입니다.<br><br><br>'
-        for bank, code in BANK_CODE.items():
-            url = 'http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth=%s&topFinGrpNo=020000&pageNo=1&financeCd=%s' % (bp.finlife_key, code)
+        cnt = 0
+        for bank, code in code_info.items():
+            url = 'http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth=%s&topFinGrpNo=020000&pageNo=1&financeCd=%s' % (bp.finlife_key, gcode, code)
             r = bp.request_and_get(url, '금융감독원_예금금리정보')
             if r is None:
                 continue
             soup = BeautifulSoup(r.text, 'html.parser')
             js = json.loads(str(soup))
+            if len(js['result']['baseList']) == 0:
+                continue
             result = '%s<h3>%s</h3><br><table border="1" cellspacing="0" cellpadding="3" bordercolor="#  999999" style="border-collapse:collapse">' % (result, bank)
             result = '''%s<tr>
             <th>금융상품,가입방법</th>
@@ -186,7 +205,7 @@ class DailyLifeAndPost:
             </tr>''' % (result)
             for banks in js['result']['baseList']:
                 result = '%s<tr>' % result
-                result = '%s<td><font color="red">%s</font><br><br>➡ %s으로 가입<br>➡ %s<br>➡ %s</td>' % (result, banks["fin_prdt_nm"], banks['join_way'], self.join_deny(banks['join_deny']), self.dcls_end_day(banks['dcls_end_day']))
+                result = '%s<td><font color="red">%s</font><br><br>➡ %s 가입<br>➡ %s<br>➡ %s</td>' % (result, banks["fin_prdt_nm"], banks['join_way'], self.join_deny(banks['join_deny']), self.dcls_end_day(banks['dcls_end_day']))
                 # result = '%s<td>%s</td>' % (result, banks["join_way"])
                 result = '%s<td>%s</td>' % (result, banks["spcl_cnd"].replace('\n', '<br>'))
                 # result = '%s<td>%s</td>' % (result, banks['join_member'])
@@ -194,6 +213,13 @@ class DailyLifeAndPost:
                 result = '%s<td>%s</td>' % (result, self.max_limit(banks['max_limit']))
                 result = '%s</tr>' % result
             result = '%s</table><br><br><br>' % result
+            cnt += 1
+            if group == '은행':
+                if cnt % 3 == 0:
+                    result = '%s<br>%s<br><br><br>' % (result, ADSENSE_MIDDLE)
+            else:
+                if cnt % 5 == 0:
+                    result = '%s<br>%s<br><br><br>' % (result, ADSENSE_MIDDLE)
         return result
 
     def get_specipic_mortgage_loan(self, bp, gcode, group):
@@ -201,10 +227,8 @@ class DailyLifeAndPost:
             code_info = BANK_CODE
         elif group == '저축은행':
             code_info = SAVINGS_BANK_CODE
-        elif group == '보험':
+        elif group == '보험사':
             code_info = INSURANCE_CODE
-        elif group == '금융투자':
-            code_info = INVESTMENT_CODE
         else:
             bp.logger.error('Invalid group %s', group)
             return None
@@ -249,10 +273,133 @@ class DailyLifeAndPost:
             result = '%s</table><br><br><br>' % result
         return result
 
+    def get_specipic_private_loan(self, bp, gcode, group):
+        if group == '은행':
+            code_info = BANK_CODE
+        elif group == '여신전문':
+            code_info = LOAN_CODE
+        elif group == '저축은행':
+            code_info = SAVINGS_BANK_CODE
+        elif group == '보험사':
+            code_info = INSURANCE_CODE
+        elif group == '금융투자사':
+            code_info = INVESTMENT_CODE
+        else:
+            bp.logger.error('Invalid group %s', group)
+            return None
+
+        result = '금융감독원의 금융상품통합 비교공시 정보를 바탕으로 작성된 글입니다.<br><br><br>'
+        cnt = 0
+        for bank, code in code_info.items():
+            url = 'http://finlife.fss.or.kr/finlifeapi/creditLoanProductsSearch.json?auth=%s&topFinGrpNo=%s&pageNo=1&financeCd=%s' % (bp.finlife_key, gcode, code)
+            r = bp.request_and_get(url, '금융감독원_개인신용대출정보')
+            if r is None:
+                continue
+            soup = BeautifulSoup(r.text, 'html.parser')
+            js = json.loads(str(soup))
+            if len(js['result']['baseList']) == 0:
+                continue
+            option_list_len = (len(js['result']['optionList']))
+            result = '%s<h3>%s</h3><br><table border="1" cellspacing="0" cellpadding="3" bordercolor="#  999999" style="border-collapse:collapse">' % (result, bank)
+            result = '''%s<tr>
+             <th>대출상품</th>
+             <th>금리</th>
+             </tr>''' % (result)
+            for banks in js['result']['baseList']:
+                result = '%s<tr>' % result
+                result = '%s<td><font color="red">%s</font><br><br>➡ %s으로 가입<br>➡ %s<br></td>' % (
+                         result,
+                         banks["crdt_prdt_type_nm"],
+                         banks['join_way'],
+                         self.dcls_end_day(banks['dcls_end_day']))
+                fin_prdt_cd = banks['fin_prdt_cd']
+                result = '%s<td>' % result
+                for i in range(option_list_len):
+                    if fin_prdt_cd == js['result']['optionList'][i]['fin_prdt_cd']:
+
+                        result = '%s<br>[%s]<br>- 은행:1~2등급,비은행:1~3등급 ➡ %s<br>- 은행:3~4등급,비은행:4등급➡ %s<br>- 은행:5~6등급,비은행:5등급➡ %s<br>- 은행:7~8등급,비은행:6등급➡ %s<br>- 은행:9~10등급,비은행:7~10등급➡ %s<br>' % (
+                                 result,
+                                 js['result']['optionList'][i]['crdt_lend_rate_type_nm'],
+                                 js['result']['optionList'][i]['crdt_grad_1'],
+                                 js['result']['optionList'][i]['crdt_grad_4'],
+                                 js['result']['optionList'][i]['crdt_grad_5'],
+                                 js['result']['optionList'][i]['crdt_grad_6'],
+                                 js['result']['optionList'][i]['crdt_grad_10'])
+                result = '%s</td></tr>' % result
+            result = '%s</table><br><br><br>' % result
+            cnt += 1
+            if cnt % 5 == 0:
+                result = '%s<br>%s<br><br><br>' % (result, ADSENSE_MIDDLE)
+        return result
+
+    def get_specipic_rent_subsidy(self, bp, gcode, group):
+        if group == '은행':
+            code_info = BANK_CODE
+        elif group == '여신전문':
+            code_info = LOAN_CODE
+        elif group == '저축은행':
+            code_info = SAVINGS_BANK_CODE
+        elif group == '보험사':
+            code_info = INSURANCE_CODE
+        elif group == '금융투자사':
+            code_info = INVESTMENT_CODE
+        else:
+            bp.logger.error('Invalid group %s', group)
+            return None
+
+        result = '금융감독원의 금융상품통합 비교공시 정보를 바탕으로 작성된 글입니다.<br><br><br>'
+        cnt = 0
+        for bank, code in code_info.items():
+            url = 'http://finlife.fss.or.kr/finlifeapi/rentHouseLoanProductsSearch.json?auth=%s&topFinGrpNo=%s&pageNo=1&financeCd=%s' % (bp.finlife_key, gcode, code)
+            r = bp.request_and_get(url, '금융감독원_전세자금대출정보')
+            if r is None:
+                continue
+            soup = BeautifulSoup(r.text, 'html.parser')
+            js = json.loads(str(soup))
+            if len(js['result']['baseList']) == 0:
+                continue
+            option_list_len = (len(js['result']['optionList']))
+            result = '%s<h3>%s</h3><br><table border="1" cellspacing="0" cellpadding="3" bordercolor="#  999999" style="border-collapse:collapse">' % (result, bank)
+            result = '''%s<tr>
+             <th>대출상품</th>
+             <th>대출부대비용</th>
+             <th>그외 수수료</th>
+             <th>금리</th>
+             </tr>''' % (result)
+            for banks in js['result']['baseList']:
+                result = '%s<tr>' % result
+                result = '%s<td><font color="red">%s</font><br><br>➡ %s으로 가입<br>➡ 제한: <strong>%s</strong><br>➡ %s<br></td>' % (
+                         result,
+                         banks["fin_prdt_nm"],
+                         banks['join_way'],
+                         banks['loan_lmt'],
+                         self.dcls_end_day(banks['dcls_end_day']))
+                result = '%s<td>%s</td>' % (result, banks["loan_inci_expn"].replace('\n', '<br>'))
+                result = '%s<td>[중도상환수수료]<br>%s<br><br>[연체 이자율]%s<br></td>' % (
+                         result,
+                         banks["loan_inci_expn"].replace('\n', '<br>'),
+                         banks["erly_rpay_fee"].replace('\n', '<br>'))
+                fin_prdt_cd = banks['fin_prdt_cd']
+                result = '%s<td>' % result
+                for i in range(option_list_len):
+                    if fin_prdt_cd == js['result']['optionList'][i]['fin_prdt_cd']:
+                        result = '%s<br>[%s]<br>%s<br><br>최저: %s<br>최고: %s<br> ' % (
+                                 result,
+                                 js['result']['optionList'][i]['rpay_type_nm'],
+                                 js['result']['optionList'][i]['lend_rate_type_nm'],
+                                 js['result']['optionList'][i]['lend_rate_min'],
+                                 js['result']['optionList'][i]['lend_rate_max'])
+                result = '%s</td></tr>' % result
+            result = '%s</table><br><br><br>' % result
+            cnt += 1
+            if cnt % 5 == 0:
+                result = '%s<br>%s<br><br><br>' % (result, ADSENSE_MIDDLE)
+        return result
+
     def mortgage_loan(self, bp):
         grp_code = {'020000': '은행',
                     '030300': '저축은행',
-                    '050000': '보험', }
+                    '050000': '보험사', }
         # 030200(여신전문),  060000(금융투자)
         for gcode, group in grp_code.items():
 
@@ -262,11 +409,129 @@ class DailyLifeAndPost:
                 continue
             bp.tistory_post('dexa', title, content, '731649')
 
-    def dividend_income(self, bp, rankTpcd, stkTpcd = '1'):  # 주식 배당 관련 조회
+    def private_loan(self, bp):
+        grp_code = {'020000': '은행',
+                    '030200': '여신전문',
+                    '030300': '저축은행',
+                    '050000': '보험사', }
+        for gcode, group in grp_code.items():
+
+            title = '[%s] %s 개인신용대출 금리 정보' % (bp.today, group)
+            content = self.get_specipic_private_loan(bp, gcode, group)
+            if content is None:
+                continue
+            bp.tistory_post('dexa', title, content, '731649')
+
+    def rent_subsidy(self, bp):
+        grp_code = {'020000': '은행',
+                    '030300': '저축은행',
+                    '050000': '보험사', }
+
+        for gcode, group in grp_code.items():
+
+            title = '[%s] %s 전세자금대출 금리 정보' % (bp.today, group)
+            content = self.get_specipic_rent_subsidy(bp, gcode, group)
+            if content is None:
+                continue
+            bp.tistory_post('dexa', title, content, '731649')
+
+    def get_specipic_savings(self, bp, gcode, group):
+        if group == '은행':
+            code_info = BANK_CODE
+        elif group == '여신전문':
+            code_info = LOAN_CODE
+        elif group == '저축은행':
+            code_info = SAVINGS_BANK_CODE
+        elif group == '보험사':
+            code_info = INSURANCE_CODE
+        elif group == '금융투자사':
+            code_info = INVESTMENT_CODE
+        else:
+            bp.logger.error('Invalid group %s', group)
+            return None
+
+        result = '금융감독원의 금융상품통합 비교공시 정보를 바탕으로 작성된 글입니다.<br><br><br>'
+        cnt = 0
+        for bank, code in code_info.items():
+            url = 'http://finlife.fss.or.kr/finlifeapi/savingProductsSearch.json?auth=%s&topFinGrpNo=%s&pageNo=1&financeCd=%s' % (bp.finlife_key, gcode, code)
+            r = bp.request_and_get(url, '금융감독원_적금금리정보')
+            if r is None:
+                continue
+            soup = BeautifulSoup(r.text, 'html.parser')
+            js = json.loads(str(soup))
+            if len(js['result']['baseList']) == 0:
+                continue
+            option_list_len = (len(js['result']['optionList']))
+            result = '%s<h3>%s</h3><br><table border="1" cellspacing="0" cellpadding="3" bordercolor="#999999" style="  border-collapse:collapse">' % (result, bank)
+            result = '''%s<tr>
+                       <th width="150">적금상품</th>
+                       <th>우대조건</th>
+                       <th width="120">이자</th>
+                       </tr>''' % (result)
+            for banks in js['result']['baseList']:
+                result = '%s<tr>' % result
+                result = '%s<td><font color="red">%s</font><br><br>➡ %s으로 가입<br>➡ %s<br>➡ %s<br>➡ %s<br><br>➡ <strong>한도: 월 %s</strong></td>' % (
+                         result,
+                         banks["fin_prdt_nm"],
+                         banks['join_way'],
+                         banks['join_member'],
+                         self.join_deny(banks['join_deny']),
+                         self.dcls_end_day(banks['dcls_end_day']),
+                         self.max_limit(banks['max_limit']))
+
+                result = '%s<td>%s</td>' % (result, banks["spcl_cnd"].replace('\n', '<br>'))
+
+                fin_prdt_cd = banks['fin_prdt_cd']
+                result = '%s<td>' % result
+                for i in range(option_list_len):
+                    if fin_prdt_cd == js['result']['optionList'][i]['fin_prdt_cd']:
+                        result = '%s<br><strong>[%s, %s]</strong><br>납입기간: %s개월<br><font color="red">저축금리: %s</font><br>최고우대금리: %s<br>' % (
+                                 result,
+                                 js['result']['optionList'][i]['intr_rate_type_nm'],
+                                 js['result']['optionList'][i]['rsrv_type_nm'],
+                                 js['result']['optionList'][i]['save_trm'],
+                                 js['result']['optionList'][i]['intr_rate'],
+                                 js['result']['optionList'][i]['intr_rate2'])
+                result = '%s</td>' % result
+
+            result = '%s</table><br><br><br>' % result
+            cnt += 1
+            if group == '은행':
+                if cnt % 3 == 0:
+                    result = '%s<br>%s<br><br><br>' % (result, ADSENSE_MIDDLE)
+            else:
+                if cnt % 5 == 0:
+                    result = '%s<br>%s<br><br><br>' % (result, ADSENSE_MIDDLE)
+        return result
+
+    def savings(self, bp):
+        grp_code = {'020000': '은행',
+                    '030300': '저축은행', }
+        for gcode, group in grp_code.items():
+
+            title = '[%s] %s 적금 금리 정보' % (bp.today, group)
+            content = self.get_specipic_savings(bp, gcode, group)
+            if content is None:
+                continue
+            bp.tistory_post('dexa', title, content, '731649')
+
+    def fixed_deposit(self, bp):
+
+        grp_code = {'020000': '은행',
+                    '030300': '저축은행', }
+        for gcode, group in grp_code.items():
+
+            title = '[%s] %s 예금 금리 정보' % (bp.today, group)
+            content = self.get_fixed_deposit(bp, gcode, group)
+            if content is None:
+                continue
+            bp.tistory_post('dexa', title, content, '731649')
+
+    def dividend_income(self, bp, rankTpcd, stkTpcd='1'):  # 주식 배당 관련 조회
         stkTpcd = '1'  # [1]보통주, [2]우선주
         listTpcd = { '11': '유가증권시장', '12': '코스닥시장',
                      '13': 'K-OTC',  '14': '코넥스시장', '50': '기타비상장', }
-        result =''
+        result = ''
         for tcode, tname in listTpcd.items():
             if rankTpcd == '1':  # [1]시가배당율, [2]액면가배당율
                 result = '%s<br><br><br><h3>%s 보통주 (시가배당율 순위)</h3><br><table border="1" cellspacing="0" cellpadding="3" bordercolor="#999999" style="  border-collapse:collapse">' % (result, tname)
@@ -307,43 +572,50 @@ class DailyLifeAndPost:
 
         return result
 
-    # def rent_subsidy(bp):
-    #     return
-
     def dexa(self, bp):
-        return
-        title = '[%s] 보통주 시가배당율 순위' % bp.today
-        content = self.dividend_income(bp, '1')
-        bp.tistory_post('dexa', title, content, '731649')
-    
-        title = '[%s] 보통주 액면가배당율 순위' % bp.today
-        content = self.dividend_income(bp, '2')
-        bp.tistory_post('dexa', title, content, '731649')
+        if bp.week_num == 0:  # monday
 
-        title = '[%s] 우선주 시가배당율 순위' % bp.today
-        content = self.dividend_income(bp, '1', '2')
-        bp.tistory_post('dexa', title, content, '731649')
+            self.rent_subsidy(bp)  # 전세자금대출
 
-        title = '[%s] 우선주 액면가배당율 순위' % bp.today
-        content = self.dividend_income(bp, '2', '2')
-        bp.tistory_post('dexa', title, content, '731649')
+            title = '[%s] 보통주 시가배당율 순위' % bp.today
+            content = self.dividend_income(bp, '1')
+            bp.tistory_post('dexa', title, content, '731649')
 
-        title = '[%s] 롯데백화점 각 지점별 문화센터 일정' % bp.today
-        content = self.lotte_curture_center(bp)
-        bp.tistory_post('dexa', title, content, '730606')
+        elif bp.week_num == 1:
 
-        # self.rent_subsidy(bp)
-        # return
-        self.mortgage_loan(bp)
+            self.private_loan(bp)  # 신용대출
 
-        title = '[%s] 정기예금 금리 정보' % bp.today
-        content = self.fixed_deposit(bp)
-        bp.tistory_post('dexa', title, content, '731649')
+            title = '[%s] 보통주 액면가배당율 순위' % bp.today
+            content = self.dividend_income(bp, '2')
+            bp.tistory_post('dexa', title, content, '731649')
 
-        title = '[%s] 빅마켓 지점별 휴관일, 영업시간, 주소, 연락처 정보' % bp.today
-        content = self.vic_market(bp)
-        bp.tistory_post('dexa', title, content, '730606')
+        elif bp.week_num == 2:
 
-        title = '[%s] 현대백화점 각 지점별 문화센터 추천강좌 일정' % bp.today
-        content = self.hyundai_curture_center(bp)
-        bp.tistory_post('dexa', title, content, '730606')
+            self.mortgage_loan(bp)  # 주택담보대출
+
+            title = '[%s] 우선주 시가배당율 순위' % bp.today
+            content = self.dividend_income(bp, '1', '2')
+            bp.tistory_post('dexa', title, content, '731649')
+
+        elif bp.week_num == 3:
+
+            self.savings(bp)  # 적금
+
+            title = '[%s] 우선주 액면가배당율 순위' % bp.today
+            content = self.dividend_income(bp, '2', '2')
+            bp.tistory_post('dexa', title, content, '731649')
+
+        elif bp.week_num == 4:
+
+            self.fixed_deposit(bp)  # 예금
+
+            title = '[%s] 롯데백화점 각 지점별 문화센터 일정' % bp.today
+            content = self.lotte_curture_center(bp)
+            bp.tistory_post('dexa', title, content, '730606')
+
+            title = '[%s] 현대백화점 각 지점별 문화센터 추천강좌 일정' % bp.today
+            content = self.hyundai_curture_center(bp)
+            bp.tistory_post('dexa', title, content, '730606')
+            # title = '[%s] 빅마켓 지점별 휴관일, 영업시간, 주소, 연락처 정보' % bp.today
+            # content = self.vic_market(bp)
+            # bp.tistory_post('dexa', title, content, '730606')
